277d0c1791782de34a3235bbd191f0974e69d2b6
TornovDutta <tornovdutta@gmail.com>
2025-11-08 00:13:44 +0530

    first commit

---
+++ src/main/java/org/example/documentshub/DocumentshubApplication.java
@@ -0,0 +1,13 @@
+package org.example.documentshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentshubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentshubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/documentshub/DocumentshubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.documentshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentshubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
